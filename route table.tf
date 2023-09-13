338726cd2998ba956a599d23899ed89e36361fea
3MR-fat7y <141360953+3MR-fat7y@users.noreply.github.com>
2023-09-13 17:48:46 +0300

    Add new file to <branch_name>

--- route table.tf
+++ route table.tf
@@ -3,7 +3,6 @@
 
 resource "aws_route_table" "igw-route-table-public" {
   vpc_id = aws_vpc.my_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
@@ -13,6 +12,21 @@ resource "aws_route_table" "igw-route-table-public" {
   }
 
 }
+
+##############################---create-private-route-table---##############################
+
+resource "aws_route_table" "nat-route-table-private" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.my_nat.id
+  }
+  tags = {
+    Name = "private_tbale"
+  }
+
+}
+
 
 ##############################---association-route-table---##############################
 
@@ -26,29 +40,15 @@ resource "aws_route_table_association" "b" {
   route_table_id = aws_route_table.igw-route-table-public.id
 }
 
-resource "aws_route_table" "igw-route-table-private" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.my_nat.id
-  }
-
-  tags = {
-    Name = "private_tbale"
-  }
-
-}
-##############################---association-route-table---##############################
 
 resource "aws_route_table_association" "c" {
   subnet_id      = aws_subnet.private-1.id
-  route_table_id = aws_route_table.igw-route-table-private.id
+  route_table_id = aws_route_table.nat-route-table-private.id
 }
 
 resource "aws_route_table_association" "d" {
   subnet_id      = aws_subnet.private-2.id
-  route_table_id = aws_route_table.igw-route-table-private.id
+  route_table_id = aws_route_table.nat-route-table-private.id
 }
 
 
