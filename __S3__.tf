e1c35c5e5bdd80cdb25b21b3a162567936122185
3MR-fat7y <amr.fathy.elsayed.97@gmail.com>
2024-02-16 14:48:41 +0200

    frist s3 for backend

--- __S3__.tf
+++ __S3__.tf
@@ -1,12 +1,12 @@
-resource "aws_s3_bucket" "github_actions_backend_1234" {
-  bucket = "github_actions_backend_1234"
+resource "aws_s3_bucket" "1234-github_actions_backend-4321" {
+  bucket = "1234-github_actions_backend-4321"
   lifecycle {
     prevent_destroy = false
   }
 }
 
 resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.github_actions_backend_1234.id
+  bucket = aws_s3_bucket.1234-github_actions_backend-4321.id
   versioning_configuration {
     status = "Enabled"
   }
