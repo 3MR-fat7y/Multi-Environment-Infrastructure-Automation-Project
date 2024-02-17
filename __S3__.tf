a52d3d00529838ad74ae14b6ae6544b2d1fe824f
3MR-fat7y <amr.fathy.elsayed.97@gmail.com>
2024-02-17 09:20:22 +0200

    test

--- __S3__.tf
+++ __S3__.tf
@@ -1,13 +1,4 @@
 resource "aws_s3_bucket" "1234-github_actions_backend-4321" {
-  bucket = "1234-github_actions_backend-4321"
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+  bucket = "1234-github-actions-backend-4321"
 
-resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.1234-github_actions_backend-4321.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
